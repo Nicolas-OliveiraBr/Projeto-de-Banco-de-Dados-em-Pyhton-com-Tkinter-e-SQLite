40a0061851a26f9609b5fe8ee59512da1d15147a
SalosFB <147280267+SalosFB@users.noreply.github.com>
2026-01-03 19:14:41 -0300

    Window_AddUpd.py: agora é possível adicionar números a lista de números(ainda sem os tipos e sem conexão com o banco de dados), os entrys dos objetos da classe LabelEntryButton(classe usada para adicionar os números de telefone) apenas aceitam números, os botões para selecionar os números a serem deletados agora são objetos da classe TelefoneButton, foi corrigido o bug de só ser possível apagar um único número e frame_listaTelefones e removeTelBtn foram reposicionados

--- window_AddUpd.py
+++ window_AddUpd.py
@@ -53,9 +53,18 @@ class LabelEntryButton(ctk.CTkFrame):
         )
         self.label.grid(row=0, column=0, padx=(0, 10), sticky="w")
 
+        def apenasNumeros(char):
+            return char.isdigit() or char == ""
+
+        validador = window.register(apenasNumeros)
+
         self.entry = Entry(
             self,
-            placeholder
+            placeholder=placeholder
+        )
+        self.entry.configure(
+            validate="key",
+            validatecommand=(validador, "%P")
         )
 
         self.entry.grid(row=0, column=1, sticky="ew", padx=(6, 10), ipadx=5)
@@ -119,6 +128,25 @@ class LabelCheckbox(ctk.CTkFrame):
 
         atualiza_label()
 
+class TelefoneButton(ctk.CTkButton):
+    def selecionarBotao(self):
+        global numberSlc
+        for btn in telefonesButtons:
+           btn.configure(fg_color="transparent",text_color="#D4A6B9")
+        self.configure(fg_color="#CFA4B5", text_color="#61223D")
+        numberSlc = self._text
+
+    def __init__(self, master, text):
+        super().__init__(master,
+                        text=text, 
+                        command=self.selecionarBotao, 
+                        font=ctk.CTkFont(family="Inter Medium", weight="bold"),
+                        height=23,
+                        text_color="#D4A6B9",
+                        fg_color="transparent",
+                        corner_radius=5
+                        )
+
 form = ctk.CTkFrame(window, fg_color="transparent")
 form.grid(row=0, column=0, padx=50, pady=25)
 
@@ -138,76 +166,76 @@ for i, (label, placeholder) in enumerate(labels_entry):
     LabelEntry(form, label, placeholder)\
         .grid(row=i, column=0, sticky="ew", pady=8)
 
+def adicionarNumero():
+    global telefonesButtons
+    newTel = button_addNum.entry.get()
+    telefonesStr.append(newTel)
+    for btn in telefonesButtons:
+        telefonesButtons.remove(btn)
+    criarListaTelefones()
 
-LabelEntryButton(
+button_addNum = LabelEntryButton(
     form,
     "Telefones:",
     "Ex: (85) 9 0000-0000",
     "Adicionar telefone",
     entries,
-    command=lambda:print("estou aqui")
-).grid(row=7, column=0, sticky="ew", pady=8)
+    command=lambda:adicionarNumero()
+)
+button_addNum.grid(row=7, column=0, sticky="ew", pady=8)
 
 tipo_telefone = ctk.CTkComboBox(form,
                                 values=["Celular","Fixo","WhatApp"], 
-                                width= 148, 
+                                width= 190, 
+                                state="readonly",
                                 fg_color="#FDE6F0", 
                                 border_color="#CFA4B5", 
                                 button_color="#CFA4B5", 
                                 dropdown_fg_color="#CFA4B5"
                                 )
+
 tipo_telefone.grid(row=8, column=0, sticky="w", padx=(172,0))
+tipo_telefone.set("Selecione o tipo de número")
 
-frame_telefones = ctk.CTkFrame(form, fg_color= "transparent")
-frame_telefones.grid(row=9, column=0, pady=8, padx=0, sticky="ew")
-frame_telefones.grid_columnconfigure(1,weight=1)
+# frame_telefones = ctk.CTkFrame(form, fg_color= "transparent")
+# frame_telefones.grid(row=9, column=0, pady=8, padx=0, sticky="ew")
+# frame_telefones.grid_columnconfigure(1,weight=1)
 
-frame_listaTelefones = ctk.CTkScrollableFrame(frame_telefones, width=125,height=100, fg_color="transparent",border_width=2, border_color="#CFA4B5", corner_radius=5)
-frame_listaTelefones.grid(row=0, column=0, padx=(172,10), sticky="n")
+frame_listaTelefones = ctk.CTkScrollableFrame(form,height=100, fg_color="transparent",border_width=2, border_color="#CFA4B5", corner_radius=5)
+frame_listaTelefones.grid(row=9, column=0, padx=(172,10), pady=(16,5), sticky="ew")
 frame_listaTelefones.grid_columnconfigure(0, weight=1)
 frame_listaTelefones._scrollbar.configure(height=0)
 
-telefonesStr = ["1254523452345", "9620394869381", "1243459704739"]#lista telefônica que será retirada do banco de dados
+telefonesStr = ["1234123423","132456","1325678","12341667884568"]
 telefonesButtons = []#lista de botões com os número telefônicos para que possam ser deletados
 numberSlc = []#número da lista de descarte de números telefonicos selecionado num momento
 
-def selecionarBotao(index):
+def removerTelefone():
     global numberSlc
-    for i in range(len(telefonesButtons)):
-        telefonesButtons[i].configure(fg_color="transparent",text_color="#D4A6B9")
-    telefonesButtons[index].configure(fg_color="#CFA4B5", text_color="#61223D")
-    numberSlc = [telefonesButtons[index].cget("text"),index]
-
+    telefonesStr.remove(numberSlc)
+    for btn in telefonesButtons:
+        if btn.cget("text") == numberSlc:
+            btn.destroy()
+            telefonesButtons.remove(btn)
+    
 def criarListaTelefones():
     for i,telefone in enumerate(telefonesStr):
-        btn = ctk.CTkButton(
+        btn = TelefoneButton(
             frame_listaTelefones,
-            height=23,
-            text=telefone,
-            font=ctk.CTkFont(weight="bold"),
-            text_color="#D4A6B9",
-            fg_color="transparent",
-            corner_radius=5,
-            command=lambda i=i: 
-                selecionarBotao(i)
+            text=telefonesStr[i]
         )
         btn.grid(row=i, column=0, sticky="ew")
         telefonesButtons.append(btn)
 
-def removerTelefone():
-    global numberSlc
-    telefonesStr.remove(numberSlc[0])
-    telefonesButtons[numberSlc[1]].destroy()
-
 criarListaTelefones()
 
-removeTelBtn = BtnIconText(frame_telefones, text="Remover telefone", command=lambda: removerTelefone())
-removeTelBtn.grid(row=0, column=1, sticky="new")
+removeTelBtn = BtnIconText(form, text="Remover telefone", command=lambda: removerTelefone())
+removeTelBtn.grid(row=10, column=0, padx=(190,20), sticky="new")
 
-LabelCheckbox(form, "Status:").grid(row=10, column=0, padx=5, pady=(8), sticky="nsew")
+LabelCheckbox(form, "Status:").grid(row=11, column=0, padx=5, pady=(8), sticky="nsew")
 
 buttons_frame = ctk.CTkFrame(form, fg_color="transparent")
-buttons_frame.grid(row=11, column=0, pady=(0, 8))
+buttons_frame.grid(row=12, column=0, pady=(0, 8))
 
 BtnIconText(
     buttons_frame,
